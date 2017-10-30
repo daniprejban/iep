5c70131ed0e69be87807e7fefe9cbfb25d40c8f3
daniprejban <32341593+daniprejban@users.noreply.github.com>
2017-10-30 17:32:15 +0200

    Add files via upload

---
+++ Server/main_blink.cpp
@@ -0,0 +1,59 @@
+// blink.c
+//
+// Example program for bcm2835 library
+// Blinks a pin on an off every 0.5 secs
+//
+// After installing bcm2835, you can build this
+// with something like:
+// gcc -o blink blink.c -l bcm2835
+// sudo ./blink
+//
+// Or you can test it before installing with:
+// gcc -o blink -I ../../src ../../src/bcm2835.c blink.c
+// sudo ./blink
+//
+// Author: Mike McCauley
+// Copyright (C) 2011 Mike McCauley
+// $Id: RF22.h,v 1.21 2012/05/30 01:51:25 mikem Exp $
+#include <bcm2835.h>
+#include <stdio.h>
+#include "pinclass.h"
+// Blinks on RPi Plug P1 pin 11 (which is GPIO pin 17)
+#define PIN RPI_GPIO_P1_11
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    Pinclass *pin = new Pinclass(PIN,HIGH,BCM2835_GPIO_FSEL_OUTP);
+    // If you call this, it will not actually access the GPIO
+    // Use for testing
+    bcm2835_set_debug(1);
+    if (!bcm2835_init())
+      return 1;
+    // Set the pin to be an output
+   // bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+    // Blink
+
+    printf("Ar trebui sa ma vezi\n");
+
+    while (1)
+    {
+        // Turn it on
+        //bcm2835_gpio_write(PIN, HIGH);
+        pin->Set(HIGH);
+        // wait a bit
+        bcm2835_delay(500);
+
+        // turn it off
+         pin->Set(LOW);
+        //bcm2835_gpio_write(PIN, LOW);
+
+
+        // wait a bit
+        bcm2835_delay(500);
+
+    }
+    bcm2835_close();
+    return 0;
+}
